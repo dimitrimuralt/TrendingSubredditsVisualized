a85daea641c06196abee295b6383bc322c5adc39
iuwu <dimitri.muralt@students.fhnw.ch>
2018-04-28 17:47:51 +0200

    loading multiple files, refactored css, renamed data files

--- src/lineChart.js
+++ src/lineChart.js
@@ -28,35 +28,41 @@ svg.append("text")
     .text("Most popular Subreddits");
 
 
-d3.json("./data/top10SubredditsByDate.json").get(function (error,posts) {
+d3.queue()
+    .defer(d3.json, "./data/posts.json")
+    .defer(d3.json, "./data/postDetails.json")
+    .await(analyze)
+    ;
 
-    // Format the date
-    var parseDate = d3.timeParse("%Y-%m-%d");
-    posts.forEach(function(d) {
-        d.postedDate = parseDate(d.postedDate);
-        //d.postsPerDay =+ d.postsPerDay;
-    });
+function analyze(error, posts, postDetails) {
+    if(error) { console.log(error); }
+     // Format the date
+        var parseDate = d3.timeParse("%Y-%m-%d");
+        posts.forEach(function (d) {
+            d.postedDate = parseDate(d.postedDate);
+            //d.postsPerDay =+ d.postsPerDay;
+        });
 
 
+        // Define the value domains
+        const dateDomain = d3.extent(posts, function (d) {
+            return d.postedDate;
+        });
+        const postsDomain = d3.extent(posts, d => Number(d.postsPerDay));
 
+        // Create scales for x and y direction
+        const xScale = d3.scaleTime()
+            .range([0, width])
+            .domain(dateDomain);
+        const yScale = d3.scaleLinear()
+            .rangeRound([height, 0])
+            .domain(postsDomain)
+            .nice(5);
 
-    // Define the value domains
-    const dateDomain = d3.extent(posts, function(d) { return d.postedDate; });
-    const postsDomain = d3.extent(posts, d => Number(d.postsPerDay));
-
-    // Create scales for x and y direction
-    const xScale = d3.scaleTime()
-        .range([0, width])
-        .domain(dateDomain);
-    const yScale = d3.scaleLinear()
-        .rangeRound([height,0])
-        .domain(postsDomain)
-        .nice(5);
-
-    // Create xAxis
-    const xAxis = d3.axisBottom(xScale);
-    lineChartGroup
-        .append("g")  // create a group and add axis
+        // Create xAxis
+        const xAxis = d3.axisBottom(xScale);
+        lineChartGroup
+            .append("g")  // create a group and add axis
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis
                 .tickFormat(d3.timeFormat("%Y-%m-%d")))
@@ -66,77 +72,87 @@ d3.json("./data/top10SubredditsByDate.json").get(function (error,posts) {
             .attr("dy", ".15em")
             .attr("transform", "rotate(-65)");
 
-    // Create yAxis
-    const yAxis = d3.axisLeft(yScale);
-    lineChartGroup
-        .append("g")  // create a group and add axis
-        .call(yAxis);
+        // Create yAxis
+        const yAxis = d3.axisLeft(yScale);
+        lineChartGroup
+            .append("g")  // create a group and add axis
+            .call(yAxis);
 
-    // add circles
-    const data_points = lineChartGroup.selectAll("circle")
-        .data(posts)
-        .enter().append("circle")
-            .attr("cx", function(d) { return xScale(d.postedDate); })
+        // add circles
+        const dataPoint = lineChartGroup.selectAll("circle")
+            .data(posts)
+            .enter().append("circle")
+            .attr("cx", function (d) {
+                return xScale(d.postedDate);
+            })
             .attr("cy", d => yScale(d.postsPerDay))
             .attr("r", 4)
             .style("fill", d => colorScale(d["subreddit"]));
 
-    //create line generator
-    const line = d3.line()
-        .x(function (d) {return xScale(d.postedDate);})
-        .y(function (d) {return yScale(d.postsPerDay);})
-        .curve(d3.curveCardinal)
-    ;
+        //create line generator
+        const line = d3.line()
+            .x(function (d) {
+                return xScale(d.postedDate);
+            })
+            .y(function (d) {
+                return yScale(d.postsPerDay);
+            })
+            .curve(d3.curveCardinal)
+        ;
 
-    // group date by subreddit
-    const nestedBySubreddit = d3.nest()
-        .key(function (d) {return d.subredditId; })
-        .entries(posts);
+        // group date by subreddit
+        const nestedBySubreddit = d3.nest()
+            .key(function (d) {
+                return d.subredditId;
+            })
+            .entries(posts);
 
-    // draw lines which connect all subreddits with the same name
-    nestedBySubreddit.forEach(function (d) {
-        lineChartGroup
-            .append("path")
-            .attr("d",line(d.values))
-            .attr("stroke", function (){return colorScale(d.values[0].subreddit)});
-    });
-
-    // Create tooltip
-    const tooltip = d3.select("body").append("div").classed("tooltip", true);
-    tooltip.style("visibility", "hidden");
-    const dayFormatter = d3.timeFormat("%A, %d. %B %Y%");
-
-    data_points.on("mouseover", function(d, i) {
-        tooltip
-            .html(`Subreddit: ${d["subreddit"]}<br/>`
-                + `Posts today: ${d.postsPerDay}<br/>`
-                + `Date: ${dayFormatter(d.postedDate)}<br/>`)
-            .style("visibility", "visible")
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-    })
-        .on("mouseout", function(d,i) {
-            tooltip.style("visibility", "hidden")
+        // draw lines which connect all subreddits with the same name
+        nestedBySubreddit.forEach(function (d) {
+            lineChartGroup
+                .append("path")
+                .attr("d", line(d.values))
+                .attr("stroke", function () {
+                    return colorScale(d.values[0].subreddit)
+                });
         });
 
+        // Create tooltip
+        const tooltip = d3.select("body").append("div").classed("tooltip", true);
+        tooltip.style("visibility", "hidden");
+        const dayFormatter = d3.timeFormat("%A, %d. %B %Y%");
 
-    // Text label for the y axis
-    lineChartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left / 1.5)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .attr("font-family", "sans-serif")
-        .style("text-anchor", "middle")
-        .text("Posts per day");
+        dataPoint.on("mouseover", function (d, i) {
+            tooltip
+                .html(`Subreddit: ${d["subreddit"]}<br/>`
+                    + `Posts today: ${d.postsPerDay}<br/>`
+                    + `Date: ${dayFormatter(d.postedDate)}<br/>`)
+                .style("visibility", "visible")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+            .on("mouseout", function (d, i) {
+                tooltip.style("visibility", "hidden")
+            });
 
-    // Text label for the x axis
-    lineChartGroup.append("text")
-        .attr("y", height + margin.bottom / 1.5)
-        .attr("x", width / 2)
-        .attr("dy", "1em")
-        .attr("font-family", "sans-serif")
-        .style("text-anchor", "middle")
-        .text("Date");
 
-});
+        // Text label for the y axis
+        lineChartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left / 1.5)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("font-family", "sans-serif")
+            .style("text-anchor", "middle")
+            .text("Posts per day");
+
+        // Text label for the x axis
+        lineChartGroup.append("text")
+            .attr("y", height + margin.bottom / 1.5)
+            .attr("x", width / 2)
+            .attr("dy", "1em")
+            .attr("font-family", "sans-serif")
+            .style("text-anchor", "middle")
+            .text("Date");
+
+}
